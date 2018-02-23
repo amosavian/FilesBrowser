75ed8c030461521565196c8442cee64c38288d29
Amir Abbas <a.mosavian@gmail.com>
2018-02-24 00:22:23 +0330

    Initial Commit

---
+++ FilesBrowser/FilesBrowser.h
@@ -0,0 +1,19 @@
+//
+//  FilesBrowser.h
+//  FilesBrowser
+//
+//  Created by Amir Abbas on 12/5/1396 AP.
+//  Copyright © 1396 AP Mousavian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FilesBrowser.
+FOUNDATION_EXPORT double FilesBrowserVersionNumber;
+
+//! Project version string for FilesBrowser.
+FOUNDATION_EXPORT const unsigned char FilesBrowserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FilesBrowser/PublicHeader.h>
+
+
